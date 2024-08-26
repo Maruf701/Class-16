d203d598f49a632025e3b4131caacbcd48b4c176
Maruf Khan <marufatrai12@gmail.com>
2024-08-26 20:26:07 +0600

    JS for loop

---
+++ javascript/script.js
@@ -0,0 +1,64 @@
+
+
+    // #1 — Print the numbers from 0 to 15
+/*
+    for (var x = 0; x <= 15; x++){
+
+        document.write(x + "<br>");
+
+    }
+*/
+
+
+    // #2 — Print the numbers from 12 to 24
+
+
+/*
+    for(var i = 12; i <= 24; i++){
+
+        document.write(i + "</br>");
+
+    }
+*/
+
+
+    // #3 — Print the ODD numbers from 7 to 31
+
+/*
+
+    for(var y = 7 ; y <= 31; y++){
+
+        if(y % 2 !== 0){
+
+            document.write(y + "<br>");
+
+        }
+
+    }
+*/
+
+
+
+    // #4 — Print the EVEN numbers from 10 to -20
+
+/*
+    for(var x = 10; x <= 20; x++){
+
+        if(x % 2 === 0){
+
+            document.write(x + "<br>");
+
+        }
+
+    }
+
+*/
+
+    // #5 — Print the Number form 15 to 0 
+
+
+    for(var x = 15 ; x >= 0; x--){
+
+        document.write(x + "<br>");
+
+    }
